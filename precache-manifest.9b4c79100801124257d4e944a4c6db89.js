2dc5fee08aaa44a6016dbab30e7df184b5228cbe
Jayendra Sai Varma Datla <jayendra.s.datla-1@ou.edu>
2025-06-08 17:42:08 -0500

    Updates

---
+++ precache-manifest.9b4c79100801124257d4e944a4c6db89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c69e60de998bfd885bf4fd0fc9ca27",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2b34c8fd3b4f40b232b6",
+    "url": "/portfolio/static/css/2.de1632da.chunk.css"
+  },
+  {
+    "revision": "fb5c32de919af1125343",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2b34c8fd3b4f40b232b6",
+    "url": "/portfolio/static/js/2.99e7ea4a.chunk.js"
+  },
+  {
+    "revision": "e73c609bb1bbded8a64d8ac5912767c1",
+    "url": "/portfolio/static/js/2.99e7ea4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb5c32de919af1125343",
+    "url": "/portfolio/static/js/main.51bd24cc.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  }
+]);
