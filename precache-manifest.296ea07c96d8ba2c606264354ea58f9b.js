17636d42b1c39887391b6349b2591a674cad17bf
Jayendra Sai Varma Datla <jayendra.s.datla-1@ou.edu>
2025-07-15 13:03:27 -0500

    Updates

---
+++ precache-manifest.296ea07c96d8ba2c606264354ea58f9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068f285b11937f91f3715bf8926691e0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d30908197b8e611f3927",
+    "url": "/portfolio/static/css/2.de1632da.chunk.css"
+  },
+  {
+    "revision": "7748c8599289d1b3667a",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d30908197b8e611f3927",
+    "url": "/portfolio/static/js/2.5858a5f2.chunk.js"
+  },
+  {
+    "revision": "e73c609bb1bbded8a64d8ac5912767c1",
+    "url": "/portfolio/static/js/2.5858a5f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7748c8599289d1b3667a",
+    "url": "/portfolio/static/js/main.3389f65c.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  },
+  {
+    "revision": "407ec04fcdfc6ec1c7e92a6de7c72f9f",
+    "url": "/portfolio/static/media/profilepic.407ec04f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9b4c79100801124257d4e944a4c6db89.js"
+  "/portfolio/precache-manifest.296ea07c96d8ba2c606264354ea58f9b.js"
 );
 
 self.addEventListener('message', (event) => {
